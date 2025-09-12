422cc6cfb14bf1eeea6a04f5c6b0141b3147934a
vsmdv <ecoe0379@gmail.com>
2025-09-12 09:09:35 +0530

    Update config.js

--- config.js
+++ config.js
@@ -8,5 +8,5 @@ function convertToBool(text, fault = 'true') {
 }
 
 module.exports = {
-    SESSION_ID: process.env.SESSION_ID || ''
+    SESSION_ID: process.env.SESSION_ID || 'YASIYA-MD=1203352eef5c14db'
 };
